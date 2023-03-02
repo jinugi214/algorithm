11ed37da4f7a7dc6d9bd9284a1e5691fdd7a85db
JINUK JEONG <wlsdnr214@gmail.com>
2023-03-02 11:43:56 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 192 ms, Memory: 42260 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+MAX = 100001
+cnt = [-1] * MAX
+move = [-1] * MAX
+
+q = deque()
+
+q.append(N)
+cnt[N] = 0
+
+cnt_ans = 0
+move_ans = []
+
+while q:
+    val = q.popleft()
+    if val == K:
+        cnt_ans = cnt[K]
+        break
+
+    for x in (val * 2, val + 1, val - 1):
+        if 0 <= x < MAX and cnt[x] == -1:
+            cnt[x] = cnt[val] + 1
+            move[x] = val
+            q.append(x)
+
+q = deque()
+cnt_lst = [K]
+
+for _ in range(cnt_ans):
+    cnt_lst.append(move[cnt_lst[-1]])
+
+print(cnt_ans)
+print(*cnt_lst[::-1], sep=' ')

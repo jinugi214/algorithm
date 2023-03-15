f02b174a6538896782f395e16ac122ac372ddd11
JINUK JEONG <wlsdnr214@gmail.com>
2023-03-15 17:31:08 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,33 +1,19 @@
-# i-1 -> i -> i+1
-#  N 1
-# 종이에는 -N <= 정수 <= N
-
-# 1번부터 풍선 터트리며 종이에 적혀있는 값만큼 이동하며
-# 양수일 경우 오른쪽 음수일 경우 왼쪽 이동
-
-# 번 풍선의 왼쪽에 N번 풍선이 있고, N번 풍선의 오른쪽에 1번 풍선이 있다.
-# -> 원형 큐를 이용한 문제
-
-from collections import deque
-
+import sys
+input = sys.stdin.readline
 N = int(input())
-
-input_data = list(map(int, input().split()))
-
-data = []
-
-for idx, val in enumerate(input_data):
-    data.append((idx, val))
-
+arr = list(map(int, input().split()))
+for i in range(N):
+    arr[i] = (i+1, arr[i])
 idx = 0
-
-q = deque(data)
-
-while q:
-    idx, val = q.popleft()
-    # 음수를 넣으면 오른쪽 이동, 양수를 넣으면 왼쪽 이동이 된다.
-    print(idx + 1, end=' ')
-    if val > 0:
-        q.rotate(-(val - 1))  # Argument 음수를 넣게 된다면 왼쪽 회전 양수는 오른쪽 회전
-    else:
-        q.rotate(-val)
+ans = []
+while True:
+    temp = arr.pop(idx)
+    ans.append(temp[0])
+    next = temp[1]-1    
+    N -= 1
+    if N == 0:
+        break
+    if next < 0:
+        next += N+1
+    idx = (idx+next)%N    
+print(*ans)

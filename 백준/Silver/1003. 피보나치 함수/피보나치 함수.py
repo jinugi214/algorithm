50ecce5d8e5f5763f9f3530abac5b15e5f50e66f
JINUK JEONG <wlsdnr214@gmail.com>
2023-04-09 22:24:39 +0900

    Update 피보나치 함수.py

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,13 +1,14 @@
-## 일반 풀이
+# 피보나치 함수
 def fib(N):
-    zeros=[1,0,1]
-    ones=[0,1,1]
+    zeros = [1, 0, 1] # 0이 출력되는 횟수 리스트
+    ones = [0, 1, 1] # 1이 출력되는 횟수 리스트
     if N >= 3:
-        for i in range(2,N):
-            zeros.append(zeros[i-1] + zeros[i])
-            ones.append(ones[i-1] + ones[i])
-    print(f"{zeros[N]} {ones[N]}")
- 
+        for i in range(2, N):
+            zeros.append(zeros[i - 1] + zeros[i]) # 피보나치 수열을 이용
+            ones.append(ones[i - 1] + ones[i]) # 피보나치 수열을 이용
+    print(f"{zeros[N]} {ones[N]}") # 해당 N값에서 0과 1 출력되는 횟수
+
+
 T = int(input())
 for _ in range(T):
     N = int(input())

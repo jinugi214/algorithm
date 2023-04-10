1affcb9e39507f0a997e742b1a1ad3ec44a24d78
JINUK JEONG <wlsdnr214@gmail.com>
2023-04-11 01:09:18 +0900

    Update Z.py

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,10 +1,30 @@
 N, r, c = map(int, input().split())
 
-def sol(N, r, c):
+def z(N, r, c, q):
+    if N == 0:
+        return q
+    half = 2 ** (N - 1)  # 절반의 길이
+    if r < half:
+        if c < half: # 1사분면
+            quad = 1
+        else: # 2사분면
+            quad = 2
+            c -= half # 다음 N-1에서의 위치를 위해 조정하자
 
-	if N == 0:
-		return 0
-        
-	return 2*(r%2)+(c%2) + 4*sol(N-1, int(r/2), int(c/2))
+    else:
+        if c < half: # 3사분면
+            quad = 3
+            r -= half
 
-print(sol(N, r, c))
+        else: # 4사분면
+            quad = 4
+            r -= half
+            c -= half
+    # 현재 배열에서의 위치 앞에 있었던 사분면들의 숫자들을 더하자
+    # ex) 4분면이면 3분면까지의 숫자들
+    q += (quad - 1) * (half ** 2)
+
+    # ex) 4분면이면 현재 4분면만의 배열로 다시 시작
+    return (z(N - 1, r, c, q))
+
+print(z(N, r, c, 0))

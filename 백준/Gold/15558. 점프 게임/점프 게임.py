61b843340ea59250c4c923a488d56804000fb266
JINUK JEONG <wlsdnr214@gmail.com>
2023-06-12 20:53:17 +0900

    [Gold V] Title: 점프 게임, Time: 252 ms, Memory: 35624 KB -BaekjoonHub

--- 백준/Gold/15558. 점프 게임/점프 게임.py
+++ 백준/Gold/15558. 점프 게임/점프 게임.py
@@ -5,30 +5,32 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-game_map = []
+line = []
 for _ in range(2):
-    game_map.append(list(map(int, input().rstrip())))
+    line.append(list(map(int, input().rstrip())))
+
+visited = [[False] * n for _ in range(2)]
 
 
-def visitable(x, y, idx):
-    return idx < y < n and game_map[x][y] and not visited[x][y]
+def check(x, y, idx):
+    return idx < y < n and line[x][y] and not visited[x][y]
 
 
 def bfs(start):
     q = deque([start])
+
     cur_idx = 0
     while q:
         for _ in range(len(q)):
             x, y = q.popleft()
-            for next_x, next_y in ((x, y + 1), (x, y - 1), (~x, y + k)):
-                if next_y >= n:
+            for nx, ny in ((x, y + 1), (x, y - 1), (~x, y + k)):
+                # ~x =>  0은 1로, 1은 0
+                if ny >= n:
                     return 1
-                if visitable(next_x, next_y, cur_idx):
-                    q.append((next_x, next_y))
-                    visited[next_x][next_y] = True
+                if check(nx, ny, cur_idx):
+                    q.append((nx, ny))
+                    visited[nx][ny] = True
         cur_idx += 1
     return 0
 
-if __name__ == "__main__":
-    visited = [[False] * n for _ in range(2)]
-    print(bfs((0, 0)))
+print(bfs((0, 0)))

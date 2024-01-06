e8a909024f63d7ae1d768038c0152ba6468f4bb3
JINUK JEONG <wlsdnr214@gmail.com>
2024-01-06 13:14:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 604 ms, Memory: 38604 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,64 +1,66 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
     static int N;
     static boolean[] visited;
-    static int[][] graph;
-    static int M;
-    static int V;
+    static ArrayList<Integer>[] graph;
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-        M = sc.nextInt();
-        V = sc.nextInt();
+        int M = sc.nextInt();
+        int V = sc.nextInt();
         sc.nextLine();
 
-        visited = new boolean[N+1];
-        visited[0] = true;
-        graph = new int[N+1][N+1];
-
-        for (int i = 0; i < M; i++){
-            String input = sc.nextLine();
-            String[] tmp = input.split(" ");
-            int a = Integer.parseInt(tmp[0]);
-            int b = Integer.parseInt(tmp[1]);
-            graph[a][b] = 1;
-            graph[b][a] = 1;
+        graph = new ArrayList[N+1];
+        for(int i = 1; i <= N; i++){
+            graph[i] = new ArrayList<>();
         }
 
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        visited = new boolean[N+1];
         dfs(V);
         System.out.println();
+
         visited = new boolean[N+1];
-        visited[0] = true;
         bfs(V);
     }
 
     static void dfs(int num){
         System.out.print(num + " ");
         visited[num] = true;
-        for (int i = 1; i < N+1; i++){
-            if(!visited[i] && graph[num][i] == 1){
-                dfs(i);
+        for (int next : graph[num]) {
+            if (!visited[next]) {
+                dfs(next);
             }
         }
     }
+
     static void bfs(int start){
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
         visited[start] = true;
+
         while (!queue.isEmpty()){
             int num = queue.poll();
             System.out.print(num + " ");
-            for (int i = 1; i < N+1; i++){
-                if(!visited[i] && graph[num][i] == 1){
-                    queue.add(i);
-                    visited[i] = true;
+            for (int next : graph[num]) {
+                if (!visited[next]) {
+                    queue.add(next);
+                    visited[next] = true;
                 }
             }
         }

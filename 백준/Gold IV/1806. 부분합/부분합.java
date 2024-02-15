3d322595224ab4dc6992d0a74fd1196abedccbb3
JINUK JEONG <wlsdnr214@gmail.com>
2024-02-15 16:57:44 +0900

    [Gold IV] Title: 부분합, Time: 312 ms, Memory: 22488 KB -BaekjoonHub

---
+++ 백준/Gold IV/1806. 부분합/부분합.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] array = new int[N];
+        int idx = 0;
+        while(st.hasMoreTokens()){
+            array[idx] = Integer.parseInt(st.nextToken());
+            idx++;
+        }
+
+        int left = 0;
+        int right = 0;
+
+        int answer = N + 1;
+        int val = array[left];
+        while(left <= right){
+            boolean lv = false;
+            boolean rv = false;
+            if (val >= S){
+                answer = Math.min(answer, right - left + 1);
+                lv = true;
+            } else {
+                rv = true;
+            }
+
+            if (lv){
+                val -= array[left];
+                left += 1;
+            }
+
+            if (rv){
+                right += 1;
+                if (right >= N) break;
+                val += array[right];
+            }
+
+        }
+
+        if(answer == N + 1){
+            answer = 0;
+        }
+
+        System.out.println(answer);
+    }
+}
